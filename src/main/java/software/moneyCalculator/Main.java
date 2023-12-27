899f0b8c133efe97806a914f07ac5daed40426f6
Oscar <oscarmunozhidalgo@gmail.com>
2023-12-27 17:27:33 +0000

    API to string

--- src/main/java/software/moneyCalculator/Main.java
+++ src/main/java/software/moneyCalculator/Main.java
@@ -1,11 +1,20 @@
 package software.moneyCalculator;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
     //http://data.fixer.io/api/latest?access_key=8bd70f9cec061336bbed4e1a0e4ae85d
     //fqblidsmdhwcsutwhh@cazlg.com
     //123456
     //Fixed
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://data.fixer.io/api/latest?access_key=8bd70f9cec061336bbed4e1a0e4ae85d");
+        InputStream x = url.openStream();
+        String a = new String(x.readAllBytes());
+        System.out.printf(a);
         MainFrame mainframe = new MainFrame();
         mainframe.setVisible(true);
     }
