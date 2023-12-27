bf9a1bfe47880fde826a18df72007a7dc5476632
Oscar <oscarmunozhidalgo@gmail.com>
2023-12-27 17:19:34 +0000

    new API established, some data provided

--- src/main/java/software/moneyCalculator/Main.java
+++ src/main/java/software/moneyCalculator/Main.java
@@ -2,6 +2,8 @@ package software.moneyCalculator;
 
 public class Main {
     //https://data.fixer.io/api/latest?access_key=8bd70f9cec061336bbed4e1a0e4ae85d
+    //fqblidsmdhwcsutwhh@cazlg.com
+    //123456
     public static void main(String[] args) {
         MainFrame mainframe = new MainFrame();
         mainframe.setVisible(true);
