cf510fa4f9be98ecfb521709c0396a0d470f8fa3
Oscar <oscarmunozhidalgo@gmail.com>
2023-12-27 17:23:33 +0000

    fixed API access

--- src/main/java/software/moneyCalculator/Main.java
+++ src/main/java/software/moneyCalculator/Main.java
@@ -4,6 +4,7 @@ public class Main {
     //http://data.fixer.io/api/latest?access_key=8bd70f9cec061336bbed4e1a0e4ae85d
     //fqblidsmdhwcsutwhh@cazlg.com
     //123456
+    //Fixed
     public static void main(String[] args) {
         MainFrame mainframe = new MainFrame();
         mainframe.setVisible(true);
