cfc5295912f9c01eaa8434c876684ad060eed404
Oscar <oscarmunozhidalgo@gmail.com>
2023-12-27 17:22:52 +0000

    new API established, some data provided

--- src/main/java/software/moneyCalculator/Main.java
+++ src/main/java/software/moneyCalculator/Main.java
@@ -1,7 +1,7 @@
 package software.moneyCalculator;
 
 public class Main {
-    //https://data.fixer.io/api/latest?access_key=8bd70f9cec061336bbed4e1a0e4ae85d
+    //http://data.fixer.io/api/latest?access_key=8bd70f9cec061336bbed4e1a0e4ae85d
     //fqblidsmdhwcsutwhh@cazlg.com
     //123456
     public static void main(String[] args) {
